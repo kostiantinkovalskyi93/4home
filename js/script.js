a1630327b9c1560d3836eb8c9a9a239c791d4611
kostiantinkovalskyi93 <koskov47@gmail.com>
2025-04-02 23:32:08 +0300

    add hamburger menu from codepan.io to faq section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
+// function toggleAnswer(element) {
+//     const answer = element.nextElementSibling;
+//     const toggleBtn = element.querySelector('.toggle-btn');
+//     const faqItem = element.parentElement;
+
+//     if (answer.style.display === 'block') {
+//         answer.style.display = 'none';
+//         toggleBtn.textContent = '+';
+//         faqItem.classList.remove('active');
+//     } else {
+//         answer.style.display = 'block';
+//         toggleBtn.textContent = '-';
+//         faqItem.classList.add('active');
+//     }
+// }
+
+const contactsBurger = document.querySelector("#header__contacts-burger");
+const contactsBurgerList = document.querySelector("#contacts-burger-list");
+const headerNavBurger = document.querySelector("#header__nav-burger");
+const navBurgerList = document.querySelector("#nav-burger-list");
+
+contactsBurger.addEventListener("click", () => {
+    contactsBurgerList.classList.toggle("contacts-burger-activ");
+});
+
+headerNavBurger.addEventListener("click", () => {
+    navBurgerList.classList.toggle("nav-burger-activ");
+});
+
 function toggleAnswer(element) {
     const answer = element.nextElementSibling;
-    const toggleBtn = element.querySelector('.toggle-btn');
+    const hamburger = element.querySelector('.hamburger');
     const faqItem = element.parentElement;
 
     if (answer.style.display === 'block') {
         answer.style.display = 'none';
-        toggleBtn.textContent = '+';
+        hamburger.classList.remove('is-active');
         faqItem.classList.remove('active');
     } else {
         answer.style.display = 'block';
-        toggleBtn.textContent = '-';
+        hamburger.classList.add('is-active');
         faqItem.classList.add('active');
     }
 }
+
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+    });
+});
+
